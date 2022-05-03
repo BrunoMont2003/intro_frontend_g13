cb77d69817771cdc78e1aa72c4438b65ced13be1
brunomont <brunoadrian.mont@gmail.com>
2022-05-02 20:52:43 -0500

    add:try catch

--- rockmusic_api/services/createData.js
+++ rockmusic_api/services/createData.js
@@ -1,17 +1,21 @@
 import getArtist from './getArtist'
 const createData = async () => {
-  const limit = []
-  for (let index = 112024; index < 112036; index++) {
-    if (index === 112032) {
-      continue
+  try {
+    const limit = []
+    for (let index = 112024; index < 112036; index++) {
+      if (index === 112032) {
+        continue
+      }
+      limit.push(`${index}`)
     }
-    limit.push(`${index}`)
+    const arrayArtits = []
+    for (let index = 0; index < limit.length; index++) {
+      arrayArtits.push(await getArtist(limit[index]))
+    }
+    console.log(arrayArtits)
+    return arrayArtits
+  } catch (error) {
+    console.log(error.message)
   }
-  const arrayArtits = []
-  for (let index = 0; index < limit.length; index++) {
-    arrayArtits.push(await getArtist(limit[index]))
-  }
-  console.log(arrayArtits)
-  return arrayArtits
 }
 export default createData
--- rockmusic_api/services/getArtist.js
+++ rockmusic_api/services/getArtist.js
@@ -1,8 +1,12 @@
 const getArtist = async (id = 112024) => {
-  const req = await window.fetch(
-    `https://theaudiodb.com/api/v1/json/2/artist.php?i=${id}`
-  )
-  const res = await req.json()
-  return res
+  try {
+    const req = await window.fetch(
+      `https://theaudiodb.com/api/v1/json/2/artist.php?i=${id}`
+    )
+    const res = await req.json()
+    return res
+  } catch (error) {
+    console.log(error.message)
+  }
 }
 export default getArtist
