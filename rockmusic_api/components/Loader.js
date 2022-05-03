2194d5fd334f398c0aaf8a470d42d12e432fcc6a
brunomont <brunoadrian.mont@gmail.com>
2022-05-02 21:55:29 -0500

    add: spinner loader

---
+++ rockmusic_api/components/Loader.js
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return `
+  <div class="w-100 h-100 d-flex justify-content-center align-items-center">
+  <div class="spinner"></div>
+  </div>
+    `
+}
+
+export default Loader
