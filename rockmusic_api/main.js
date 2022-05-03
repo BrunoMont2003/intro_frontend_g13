07dd09d81c063030a25e4aa5b42cd73b607d1c07
brunomont <brunoadrian.mont@gmail.com>
2022-05-02 20:41:45 -0500

    mod: link css

--- rockmusic_api/main.js
+++ rockmusic_api/main.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import './style.css'
+import './styles/style.css'
 
 import createData from './services/createData'
 
