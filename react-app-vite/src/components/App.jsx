dbcd65d079616f58680c26b06a78608e9995fa77
brunomont <brunoadrian.mont@gmail.com>
2022-05-10 21:58:46 -0500

    add: usestate hook

--- react-app-vite/src/components/App.jsx
+++ react-app-vite/src/components/App.jsx
@@ -3,13 +3,11 @@ import { Contador } from './Contador'
 // memo -> react components must be begin with uppercase
 const App = () => {
   // logic of component
-  const userName = 'Bruno'
 
   // component view (jsx)
   return (
     <div className='min-h-screen flex justify-center items-center bg-slate-900 text-white'>
-      Hello, {userName}!
-      <Contador />
+      <Contador valorInicial={55} />
     </div>
   )
 }
--- react-app-vite/src/components/Contador/index.jsx
+++ react-app-vite/src/components/Contador/index.jsx
@@ -1,5 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const Contador = () => {
-  return <div className='text-red-500'>index</div>
+export const Contador = ({ valorInicial = 0 }) => {
+  // useState -> component states
+  // [variable / state, mod function ]
+  const [numero, setNumero] = useState(valorInicial)
+  const add = (amount = 1) => {
+    setNumero(numero + amount)
+  }
+  const myClass =
+    'py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200'
+  return (
+    <section className='flex items-center justify-center flex-col gap-5'>
+      <button type='button' class={myClass} onClick={() => add()}>
+        Add
+      </button>
+      <p>{numero}</p>
+    </section>
+  )
 }
