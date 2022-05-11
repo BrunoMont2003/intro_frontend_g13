b0cc255b49d2cf587d9d2f307e6701651f2d99f2
brunomont <brunoadrian.mont@gmail.com>
2022-05-10 19:59:50 -0500

    add: tailwindcss

--- react-app-vite/src/components/App.jsx
+++ react-app-vite/src/components/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Contador } from './Contador'
 // memo -> react components must be begin with uppercase
 const App = () => {
   // logic of component
   const userName = 'Bruno'
 
   // component view (jsx)
-  return <div className='App'>Hello, {userName}!</div>
+  return (
+    <div className='min-h-screen flex justify-center items-center bg-slate-900 text-white'>
+      Hello, {userName}!
+      <Contador />
+    </div>
+  )
 }
 
 export default App
---
+++ react-app-vite/src/components/Contador/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Contador = () => {
+  return <div className='text-red-500'>index</div>
+}
